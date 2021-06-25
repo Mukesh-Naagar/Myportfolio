d24aeb3cf8a8b16c0a82785a0ebc188f2fee2711
mukeshnaagar <mukeshnaagarr@gmail.com>
2021-06-25 14:55:15 +0530

    Updates

---
+++ precache-manifest.5dd46bab48eec5a45384049e68bae9ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102be3bdd7aea6e3034276e061b8a28f",
+    "url": "/Myportfolio/index.html"
+  },
+  {
+    "revision": "5fdf6ac76d88005a94ea",
+    "url": "/Myportfolio/static/css/main.8a2d76df.chunk.css"
+  },
+  {
+    "revision": "85a753778bcb8779e80d",
+    "url": "/Myportfolio/static/js/2.c8653838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Myportfolio/static/js/2.c8653838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdf6ac76d88005a94ea",
+    "url": "/Myportfolio/static/js/main.f6bba866.chunk.js"
+  },
+  {
+    "revision": "58efaebaca9672cc39e7",
+    "url": "/Myportfolio/static/js/runtime-main.f0b5c67d.js"
+  }
+]);
